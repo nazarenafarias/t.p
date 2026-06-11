88eb495e10b70f91ed28ff8cddff073f934e8cf7
alumno <alumno@Instituto.local>
2026-06-11 10:44:39 -0300

    Update app.js

---
+++ TP5/app.js
@@ -0,0 +1,24 @@
+let n1=8
+let n2=9
+
+let suma=n1+n2
+let resta=n1-n2
+let multiplicacion=n1*n2
+let division=n1/n2
+
+alert("suma  es igual a:" + suma)
+alert("resta es igual a:"+ resta)
+alert("multiplicacion es igual a:"+ multiplicacion)
+alert("division es igual a:"+ division)
+
+let nombre='pepe'
+let apellido='mateos'
+let edad=16
+
+console.log("el alumno "+nombe+" "+apellido+" tiene "+edad)
+
+let precio1=2.000
+let precio2=2.600
+let precio3=4.000
+
+let suma=precio1+precio2+precio3
